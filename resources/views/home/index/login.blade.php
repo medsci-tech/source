81237d8990b172c9d17936fd89854f923d87c763
junfeng0202 <1628439099@qq.com>
2018-01-02 11:56:42 +0800

    '修改登入样式'

--- resources/views/home/index/login.blade.php
+++ resources/views/home/index/login.blade.php
@@ -16,26 +16,29 @@
             <div class="logo2"></div>
             <form class="form-horizontal loginForm" method="post" action="{{url('login')}}">
                 {{csrf_field()}}
-                @if(session('msg'))
-                    <div class="tips-center">{{session('msg')}}</div>
-                @endif
+
+                <div class="tips-center">@if(session('msg')) {{session('msg')}} @endif</div>
+
                 <div class="form-group">
-                    <label class="col-sm-5 control-label">手机号</label>
-                    <div class="col-sm-4">
+                    <label class="col-sm-4 control-label">手机号</label>
+                    <div class="col-sm-5">
                         <input type="text" class="form-control" name="doctor_mobile">
                         <div class="tips"></div>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="col-sm-5 control-label">密码</label>
-                    <div class="col-sm-4">
+                    <label class="col-sm-4 control-label">密码</label>
+                    <div class="col-sm-5">
                         <input type="password" class="form-control" name="password">
                         <div class="tips"></div>
                     </div>
                 </div>
-                <a href="{{url('forget')}}" class="wjmm">忘记密码?</a>
-                <a href="javascript:;" onclick="$('.loginForm').submit();" class="dl"></a>
+                <div class="form-group">
+                    <a href="{{url('forget')}}" class="wjmm">忘记密码?</a>
+
+                <a href="javascript:;" onclick="$('.loginForm').submit();" class="dl">登入</a>
                 <a href="{{url('register')}}" class="wjmm wjmm2">还没有账号？立即注册</a>
+                </div>
             </form>
         </div>
 
