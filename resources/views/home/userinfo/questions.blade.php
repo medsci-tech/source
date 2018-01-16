6b5412657ad4d94bffa3842dc69ea6883a7f329f
junfeng0202 <1628439099@qq.com>
2018-01-16 15:29:00 +0800

    重构第一次bug修改

--- resources/views/home/userinfo/questions.blade.php
+++ resources/views/home/userinfo/questions.blade.php
@@ -2,15 +2,6 @@
 
 @section('title','常见问题')
 
-@section('css')
-    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-@endsection
-
-@section('js')
-    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-@endsection
-
 @section('content')
     <div class="panel admin-panel">
     <div class="panel-head"><strong>常见问题</strong></div>
@@ -21,12 +12,12 @@
                 <div class="panel-heading">
                     <h4 class="panel-title">
                         <a data-toggle="collapse" data-parent="#accordion"
-                           href="#collapse{{ $k }}" style="display:block;">
+                           href="#collapse{{ $k }}" style="display:block;height: 100%;font-size: 20px;">
                             {{ $question->title }}
                         </a>
                     </h4>
                 </div>
-                <div id="collapse{{ $k }}" class="panel-collapse collapse @if($k === 0 )in @endif">
+                <div id="collapse{{ $k }}" class="panel-collapse collapse {{--@if($k === 0 )in @endif--}}">
                     <div class="panel-body">
                         {!! $question->content !!}
                     </div>
